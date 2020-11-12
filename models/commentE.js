4547fad2f2db3599da4df622da608bdee5c874f3
Sebastián Varón <65864501+sebohuebo@users.noreply.github.com>
2020-11-12 00:33:41 -0400

    Create commentE.js

---
+++ models/commentE.js
@@ -0,0 +1,29 @@
+var mongoose = require("mongoose");
+
+var commentESchema = new mongoose.Schema({
+    text: String,
+    createdAt: { type: Date, default: Date.now },
+    author: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+            },
+        username: String,
+        isAdmin: {type: Boolean, default:false}
+            },
+ 
+    commentsE: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment"
+        }
+    ],
+    likes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        }
+    ]
+});
+
+module.exports = mongoose.model("CommentE", commentESchema);
